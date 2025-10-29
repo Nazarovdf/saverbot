db9fc83cb03251d3a6afa09b94a2dbac5c5cc68b
Dilshodbek Nazarov <dilshodbekn188@gmail.com>
2025-10-29 16:25:28 +0500

    Update gitignore for temp files

---
+++ git_push.bat
@@ -0,0 +1,6 @@
+@echo off
+git commit -m "Initial commit: Telegram Media Downloader Bot"
+git remote add origin https://github.com/Nazarovdf/saverbot.git
+git branch -M main
+git push -u origin main
+pause
---
+++ update_git.bat
@@ -0,0 +1,5 @@
+@echo off
+git add .
+git commit -m "Update gitignore for temp files"
+git push origin main
+pause
