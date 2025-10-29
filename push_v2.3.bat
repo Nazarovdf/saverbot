fd2b42aad5d99525714a493bd2f66792c2be5b4a
Dilshodbek Nazarov <dilshodbekn188@gmail.com>
2025-10-29 18:33:34 +0500

    Remove push_v2.3.bat

--- push_v2.3.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo v2.3: MoviePy MP3, Remove Pinterest, Railway config
-git add .
-git commit -m "v2.3: MoviePy for MP3, Remove Pinterest, Railway.app deployment, Auto cleanup"
-git push origin main
-pause
